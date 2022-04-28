d47c893cd3b2f1a4cf0fd2749fa632eca1d4d020
Sasha Gerrand <sasha@duffel.com>
2022-04-28 17:25:33 +0100

    Add Order Cancellations API functionality

---
+++ tests/Duffel/Api/OrderCancellationsTest.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Duffel\Tests\Api;
+
+use Duffel\Api\OrderCancellations;
+use Duffel\Client;
+use Http\Client\Common\HttpMethodsClientInterface;
+use PHPUnit\Framework\TestCase;
+
+class TestOrderCancellations extends TestCase {
+  private $mock;
+  private $stub;
+
+  public function setUp(): void {
+    $this->mock = $this->createMock(HttpMethodsClientInterface::class);
+    $this->stub = $this->createStub(Client::class);
+    $this->stub->method('getHttpClient')
+               ->willReturn($this->mock);
+  }
+
+  public function testAllCallsGetWithExpectedUri(): void {
+    $this->mock->expects($this->once())
+               ->method('get')
+               ->with($this->equalTo('/air/order_cancellations'));
+
+    $actual = new OrderCancellations($this->stub);
+    $actual->all();
+  }
+
+  public function testShowWithIdCallsGetWithExpectedUri(): void {
+    $this->mock->expects($this->once())
+               ->method('get')
+               ->with($this->equalTo('/air/order_cancellations/some-id'));
+
+    $actual = new OrderCancellations($this->stub);
+    $actual->show('some-id');
+  }
+
+  public function testCreateWithIdCallsGetWithExpectedUri(): void {
+    $this->mock->expects($this->once())
+               ->method('post')
+               ->with(
+                 $this->equalTo('/air/order_cancellations'),
+                 $this->equalTo(['Content-Type' => 'application/json']),
+                 $this->equalTo('{"data":{"order_id":"some-id"}}'),
+               );
+
+    $actual = new OrderCancellations($this->stub);
+    $actual->create('some-id');
+  }
+
+  public function testConfirmWithIdCallsGetWithExpectedUri(): void {
+    $this->mock->expects($this->once())
+               ->method('post')
+               ->with(
+                 $this->equalTo('/air/order_cancellations/some-id/actions/confirm'),
+                 $this->equalTo([]),
+                 $this->equalTo(null),
+               );
+
+    $actual = new OrderCancellations($this->stub);
+    $actual->confirm('some-id');
+  }
+}
