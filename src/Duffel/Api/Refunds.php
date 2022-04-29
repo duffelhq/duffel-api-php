1b556a83e251237fb85ef097e7c1728eadc70a5f
Sasha Gerrand <sasha@duffel.com>
2022-04-29 14:56:00 +0100

    Add Refunds API functionality

---
+++ src/Duffel/Api/Refunds.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Duffel\Api;
+
+class Refunds extends AbstractApi {
+  public function create(array $refund) {
+    $params = [
+      'amount' => $refund['amount'],
+      'currency' => $refund['currency'],
+      'payment_intent_id' => $refund['payment_intent_id'],
+    ];
+
+    return $this->post('/payments/refunds', \array_filter($params, function ($value) {
+      return null !== $value && (!\is_string($value) || '' !== $value);
+    }));
+  }
+
+  public function show(string $id) {
+    return $this->get('/payments/refunds/'.self::encodePath($id));
+  }
+}
