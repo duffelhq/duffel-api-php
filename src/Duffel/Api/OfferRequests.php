46a4a64a60701961bc19510305329a530b71a513
Sasha Gerrand <sasha@duffel.com>
2022-04-29 17:17:27 +0100

    Address PHPStan warnings for OfferRequests

--- src/Duffel/Api/OfferRequests.php
+++ src/Duffel/Api/OfferRequests.php
@@ -5,14 +5,31 @@ declare(strict_types=1);
 namespace Duffel\Api;
 
 class OfferRequests extends AbstractApi {
+  /**
+   * @param array $parameters
+   *
+   * @return mixed
+   */
   public function all(array $parameters = []) {
     return $this->get('/air/offer_requests');
   }
 
+  /**
+   * @param string $id
+   *
+   * @return mixed
+   */
   public function show(string $id) {
     return $this->get('/air/offer_requests/'.self::encodePath($id));
   }
 
+  /**
+   * @param string $cabin_class
+   * @param array  $passengers
+   * @param array  $slices
+   *
+   * @return mixed
+   */
   public function create(string $cabin_class = "economy", array $passengers = array(), array $slices = array()) {
     $params = [
       'cabin_class' => $cabin_class,
