4d63e3af1a70ba672d0a78cefc8e86265c0a6ae1
Sasha Gerrand <sasha@duffel.com>
2022-05-03 19:31:14 +0100

    Address PHPStan warnings for Duffel\Api\OfferRequests

--- src/Duffel/Api/OfferRequests.php
+++ src/Duffel/Api/OfferRequests.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Duffel\Api;
 
+use Duffel\Exception\RuntimeException;
+
 class OfferRequests extends AbstractApi {
   /**
    * @param array $parameters
@@ -31,14 +33,29 @@ class OfferRequests extends AbstractApi {
    * @return mixed
    */
   public function create(string $cabin_class = "economy", array $passengers = array(), array $slices = array()) {
+    if ([] === $passengers) {
+      throw new RuntimeException("You must provide at least one passenger");
+    }
+
+    if ([] === $slices) {
+      throw new RuntimeException("You must provide at least one slice");
+    }
+
+    $resolver = $this->createOptionsResolver();
+    $resolver->setRequired(['departure_date', 'destination', 'origin']);
+
+    $parsedSlices = [];
+
+    foreach($slices as $slice) {
+      $parsedSlices[] = $resolver->resolve($slice);
+    }
+
     $params = [
       'cabin_class' => $cabin_class,
       'passengers' => $passengers,
-      'slices' => $slices,
+      'slices' => $parsedSlices,
     ];
 
-    return $this->post('/air/offer_requests', \array_filter($params, function ($value) {
-      return null !== $value && (!\is_string($value) || '' !== $value);
-    }));
+    return $this->post('/air/offer_requests', $params);
   }
 }
