a7ef1c3117d0c4f2cfa22369e0c83545f0bb2982
Sasha Gerrand <sasha@duffel.com>
2022-05-03 10:56:30 +0100

    Address PHPStan warnings for OrderCancellations

--- src/Duffel/Api/OrderCancellations.php
+++ src/Duffel/Api/OrderCancellations.php
@@ -5,14 +5,28 @@ declare(strict_types=1);
 namespace Duffel\Api;
 
 class OrderCancellations extends AbstractApi {
+  /**
+   * @return mixed
+   */
   public function all() {
     return $this->get('/air/order_cancellations');
   }
 
+  /**
+   * @param string $id
+   *
+   * @return mixed
+   */
   public function show(string $id) {
     return $this->get('/air/order_cancellations/'.self::encodePath($id));
   }
 
+
+  /**
+   * @param string $orderId
+   *
+   * @return mixed
+   */
   public function create(string $orderId) {
     $params = array(
       "order_id" => $orderId
@@ -23,6 +37,12 @@ class OrderCancellations extends AbstractApi {
     }));
   }
 
+
+  /**
+   * @param string $id
+   *
+   * @return mixed
+   */
   public function confirm(string $id) {
     return $this->post('/air/order_cancellations/'.self::encodePath($id).'/actions/confirm');
   }
