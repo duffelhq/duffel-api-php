30bd143fe2abe5d913eb2ebf776a610a23dc8205
Sasha Gerrand <sasha@duffel.com>
2022-05-03 18:25:14 +0100

    Address PHPStan warnings for Duffel\Client

--- src/Duffel/Client.php
+++ src/Duffel/Client.php
@@ -37,9 +37,24 @@ class Client {
   private const DEFAULT_API_VERSION = 'beta';
   private const VERSION = '0.0.0-alpha';
 
+  /**
+   * @var string
+   */
   private $accessToken;
+
+  /**
+   * @var string
+   */
   private $apiUrl;
+
+  /**
+   * @var string
+   */
   private $apiVersion;
+
+  /**
+   * @var Builder
+   */
   private $httpClientBuilder;
 
   public function __construct(Builder $httpClientBuilder = null) {
@@ -108,12 +123,12 @@ class Client {
     return new SeatMaps($this);
   }
 
-  public function getAccessToken() {
+  public function getAccessToken(): string {
     return $this->accessToken;
   }
 
-  public function setAccessToken(string $token) {
-    if ('' !== trim($token) && strlen(trim($token)) > 0) {
+  public function setAccessToken(string $token): void {
+    if ('' !== trim($token)) {
       $this->accessToken = $token;
     } else {
       throw new InvalidAccessTokenException("You need to set a token");
@@ -144,6 +159,7 @@ class Client {
   protected function getHttpClientBuilder(): Builder {
     return $this->httpClientBuilder;
   }
+
   private function getDefaultHeaders(): array {
     return array(
       "Duffel-Version" => $this->apiVersion,
