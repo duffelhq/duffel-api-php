368b187c8ced811a8bab15b33f5ccaec3c1db3f8
Sasha Gerrand <sasha@duffel.com>
2022-05-03 10:57:42 +0100

    Address PHPStan warnings for OrderChangeOffers

--- src/Duffel/Api/OrderChangeOffers.php
+++ src/Duffel/Api/OrderChangeOffers.php
@@ -5,6 +5,11 @@ declare(strict_types=1);
 namespace Duffel\Api;
 
 class OrderChangeOffers extends AbstractApi {
+  /**
+   * @param string $orderChangeRequestId
+   *
+   * @return mixed
+   */
   public function all(string $orderChangeRequestId = '') {
     if ('' === $orderChangeRequestId) {
       return $this->get('/air/order_change_offers');
@@ -13,6 +18,11 @@ class OrderChangeOffers extends AbstractApi {
     return $this->get('/air/order_change_offers?order_change_request_id='.self::encodePath($orderChangeRequestId));
   }
 
+  /**
+   * @param string $id
+   *
+   * @return mixed
+   */
   public function show(string $id) {
     return $this->get('/air/order_change_offers/'.self::encodePath($id));
   }
