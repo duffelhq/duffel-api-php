9c223cf96790ebc844945459280ac1419475e3b4
Sasha Gerrand <sasha@duffel.com>
2022-05-03 11:03:16 +0100

    Address PHPStan warnings for Payments

--- src/Duffel/Api/Payments.php
+++ src/Duffel/Api/Payments.php
@@ -5,6 +5,12 @@ declare(strict_types=1);
 namespace Duffel\Api;
 
 class Payments extends AbstractApi {
+  /**
+   * @param string $orderId
+   * @param array  $payment
+   *
+   * @return mixed
+   */
   public function create(string $orderId, array $payment) {
     $params = [
       'order_id' => $orderId,
