a318c73561188f975c8e6aacf574bc5eb31e3adc
Sasha Gerrand <sasha@duffel.com>
2022-05-03 11:04:50 +0100

    Address PHPStan warnings for SeatMaps

--- src/Duffel/Api/SeatMaps.php
+++ src/Duffel/Api/SeatMaps.php
@@ -5,6 +5,11 @@ declare(strict_types=1);
 namespace Duffel\Api;
 
 class SeatMaps extends AbstractApi {
+  /**
+   * @param string $offerId
+   *
+   * @return mixed
+   */
   public function all(string $offerId) {
     return $this->get('/air/seat_maps?offer_id='.self::encodePath($offerId));
   }
